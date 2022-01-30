afcf8c47be4b6e2edccd0aa35c52d16447339737
Ben Gaull <40971444+BenjaminGaull195@users.noreply.github.com>
2022-01-29 19:49:47 -0800

    Update GenCAIntermediate.sh

--- CertGeneration/CA/GenCAIntermediate.sh
+++ CertGeneration/CA/GenCAIntermediate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+CA_DIR=CA
+CA_DEBUG_DIR=$CA_DIR/ca-debug
+INTERMEDIATE_DIR=$CA_DIR/Intermediate
+INTERMEDIATE_DEBUG_DIR=$INTERMEDIATE_DIR/intermediate-debug
+
 # CA intermediate
 
 <<COMMENTS
@@ -24,13 +29,16 @@ if test -f "ca.crt" ;
 then
     echo "CA cert exists, generating intermediate certificate"
 
-    openssl req -new -nodes -text -out intermediate.csr \
-        -keyout intermediate.key -sha256 -config intermediate.cnf
-    chmod og-rwx intermediate.key
-    openssl x509 -req -sha256 -in intermediate.csr -text -days 3650 \
-        -extfile /etc/ssl/openssl.cnf -extensions v3_ca \
-        -CA ca.crt -CAkey ca.key -CAcreateserial \
-        -out intermediate.crt
+    # CA intermediate
+    openssl req -new -nodes -text -out $INTERMEDIATE_DIR/intermediate.csr \
+      -keyout $INTERMEDIATE_DIR/intermediate.key -sha256 -config intermediate.cnf
+    chmod og-rwx $INTERMEDIATE_DIR/intermediate.key
+    openssl x509 -req -sha256 -in $INTERMEDIATE_DIR/intermediate.csr -text -days 3650 \
+      -extfile /etc/ssl/openssl.cnf -extensions v3_ca \
+      -CA $CA_DIR/ca.crt -CAkey $CA_DIR/ca.key -CAcreateserial \
+      -out $INTERMEDIATE_DIR/intermediate.crt
+
+openssl pkcs -export -i $INTERMEDIATE_DIR/intermediate.crt -inkey $INTERMEDIATE_DIR/intermediate.key -out $INTERMEDIATE_DIR/intermediate.pfx
 else
     echo "CA cert not found, generating CA certificate"
     if test -f "GenCA.sh" ;
@@ -42,19 +50,8 @@ else
         echo "Failed to find GenCA, exiting"
         exit 1
     fi
-
-    echo "Generating intermediate certificate"
-
-    openssl req -new -nodes -text -out intermediate.csr \
-        -keyout intermediate.key -sha256 -config intermediate.cnf
-    chmod og-rwx intermediate.key
-    openssl x509 -req -sha256 -in intermediate.csr -text -days 3650 \
-        -extfile /etc/ssl/openssl.cnf -extensions v3_ca \
-        -CA ca.crt -CAkey ca.key -CAcreateserial \
-        -out intermediate.crt
 fi
-
 echo "Printing output of intermediate.csr to intermediate-csr-debug.txt for debug"
-openssl req -in intermediate.csr -text -noout > intermediate-debug/intermediate-csr-debug.txt
+openssl req -in $INTERMEDIATE_DIR/intermediate.csr -text -noout > $INTERMEDIATE_DEBUG_DIR/intermediate-csr-debug.txt
 echo "Printing output of intermediate.crt to intermediate-crt-debug.txt for debug"
-openssl x509 -in intermediate.crt -text -noout > intermediate-debug/intermediate-crt-debug.txt
+openssl x509 -in $INTERMEDIATE_DIR/intermediate.crt -text -noout > $INTERMEDIATE_DEBUG_DIR/intermediate-crt-debug.txt
